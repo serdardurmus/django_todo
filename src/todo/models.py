fccb7897328632dc4cee5c3edf98dfac42ba3e09
Serdar <serdar83durmus@gmail.com>
2021-01-10 15:02:56 +0100

    Added Todo models

--- src/todo/models.py
+++ src/todo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    created_date = models.DateTimeField(auto_now_add=True)  # otomatik tarih kaydediyor
+    completed = models.BooleanField(default=False)
+    
+    def __str__(self):
+        return self.title
--- src/todo/views.py
+++ src/todo/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from .models import Todo
 # Create your views here.
 
 def home(request):
     return render(request, "todo/home.html")
+
+def todo_list(request):
+    todos = Todo.objects.all()
